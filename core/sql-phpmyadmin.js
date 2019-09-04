af1511fecf60d2ae1685038ed5d0d163516ef6ce
bunticj <bunticj11@gmail.com>
2019-09-04 15:50:08 +0200

    sql

--- core/sql-phpmyadmin.js
+++ core/sql-phpmyadmin.js
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS dbforum.user (
  email TEXT NOT NULL , 
 password TEXT NOT NULL , 
 user_created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
- PRIMARY KEY (id), UNIQUE UN_KEY (email(256)) ENGINE = InnoDB;
+ PRIMARY KEY (id), UNIQUE UN_KEY (email(256))) ENGINE = InnoDB;
 
 //topic table
 CREATE TABLE IF NOT EXISTS dbforum.topic ( 
@@ -20,7 +20,7 @@ topic_created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
  PRIMARY KEY (topic_id)) ENGINE = InnoDB;
 
 //comment table
-CREATE TABLE dbforum.comment ( 
+CREATE TABLE IF NOT EXISTS dbforum.comment ( 
 comment_id INT NOT NULL AUTO_INCREMENT ,
 topic_id INT NOT NULL ,
 user_id INT NOT NULL ,
